f867730531be5010ba442ff9a73ca0581fa577d2
agruner <agruner@sesame-ed79be29.(none)>
2008-08-28 08:35:35 -0700

    adding basic layout to head.php as the start of a new site

--- head.php
+++ head.php
@@ -2,16 +2,28 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-    <?php meta_tags() ?>
-	<?php page_title() ?>
-	<script src="scripts/swfobject.js" type="text/javascript"></script>
+	<?php meta_tags() ?>
+	<script src="scripts/jquery.js" type="text/javascript"></script>
 	<script src="scripts/global.js" type="text/javascript"></script>
-	<link href="images/favicon.ico" rel="shortcut icon" />
-	<link href="styles.css" media="screen" rel="stylesheet" type="text/css" />
+	<link rel="shortcut icon" href="favicon.ico" />
+	<link href="stylesheets/main.css" media="screen" rel="stylesheet" type="text/css" />
 	<!--[if IE]><link rel="stylesheet" href="stylesheets/ie.css" type="text/css" media="screen, projection"><![endif]-->
+	<?php page_title() ?>
 </head>
 
-<body>
-<?php //main_navigation($_section, $exclusions = array('Contact Us','Site Map')) ?>
-<?php //sub_navigation($_section, $_page_name) ?>
-<?php //text_navigation() ?>
+<body class="<?php echo slug_name($_page_name); ?>">
+<?php //full_navigation($exclusions = array('Contact Us','Site Map')); ?>
+<?php //sub_navigation(); ?>
+<?php //text_navigation(); ?>
+<?php //place_image_if_alt(); ?>
+
+<div id="container">
+	<div id="hd"></div><!--end hd-->
+	<div id="bd">
+		<div id="content"></div><!--end content-->
+		<div id="sidebar"></div><!--end hd-->
+	</div><!--end bd-->
+	<div id="ft"></div><!--end ft-->
+	<div id="util"></div><!--end util-->
+	<div id="nav"></div><!--end nav-->
+</div><!--end container-->
