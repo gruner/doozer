234ae4c812bd9f240e32d4e1fe01a36150447917
agruner <agruner@sesame-ed79be29.(none)>
2008-12-11 17:27:43 -0800

    fixed issue with sitemap() in global.php. It accedentally got 'transposed' in e by the 'ctrl T' command. I must have thought Firefox was the active window at the time and didn't notice the corrupted code.

--- global.php
+++ global.php
@@ -566,40 +566,41 @@ function section_index($section="") {
  * echoes a formatted sitemap in the form of nested lists with links to each page
  * @see sitemap.php
  */
-}
-		echo $sitemap_string;
-		$sitemap_string .= '</ul>';
-		}
-			}
-				$sitemap_string .= '</li>'; # close section <li>
-				}
-						$sitemap_string .= '</ul>';
-						}
-								}
-										$sitemap_string .= "<li><a href=\"$sub_stub.php\">$sub_name</a></li>";
-								} else {
-										$sitemap_string .= "<li>$sub_name (This Page)</li>";
-								if ($sub_name == $_page_name) {
-								# Mark the current page, don't create a self referencing link
-								$sub_stub = slug_name($sub_name);
-						$sitemap_string .= '<ul>';
-						foreach ($sub_items as $sub_name) {
-				if (has_sub_items($section)) { #don't create nested ul if the only sub item is the same page
-				}
-					$sitemap_string .= "<li><a href=\"$link\">$section</a>"; #leave <li> open
-					$link = section_link($section);
-				} else {
-					$sitemap_string .= "<li>$section (This Page)"; #leave <li> open
-				if ($section == $_page_name) {
-			if (!in_array($section, $exclusions)) {
-		  # skip any sections that are in the exclusions array
-		foreach ($sitemap as $section => $sub_items) {
-		$sitemap_string = '<ul class="sitemap">';
-		$sitemap = define_sitemap();
+function sitemap($exclusions=array()) {
 		
 		global $_page_name;  
 		
-function sitemap($exclusions=array()) {
+		$sitemap = define_sitemap();
+		$sitemap_string = '<ul class="sitemap">';
+		foreach ($sitemap as $section => $sub_items) {
+		  # skip any sections that are in the exclusions array
+			if (!in_array($section, $exclusions)) {
+				if ($section == $_page_name) {
+					$sitemap_string .= "<li>$section (This Page)"; #leave <li> open
+				} else {
+					$link = section_link($section);
+					$sitemap_string .= "<li><a href=\"$link\">$section</a>"; #leave <li> open
+				}
+				if (has_sub_items($section)) { #don't create nested ul if the only sub item is the same page
+						$sitemap_string .= '<ul>';
+						foreach ($sub_items as $sub_name) {
+								$sub_stub = slug_name($sub_name);
+								# Mark the current page, don't create a self referencing link
+								if ($sub_name == $_page_name) {
+										$sitemap_string .= "<li>$sub_name (This Page)</li>";
+								} else {
+										$sitemap_string .= "<li><a href=\"$sub_stub.php\">$sub_name</a></li>";
+								}
+						}
+						$sitemap_string .= '</ul>';
+				}
+				$sitemap_string .= '</li>'; # close section <li>
+			}
+		}
+		$sitemap_string .= '</ul>';
+		echo $sitemap_string;
+}
+
 
 /**
  * echoes a formatted string with links to the current page's parent(s).
