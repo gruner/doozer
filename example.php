76ebfbebcde0a53435c7a35f836b273cb2356eb6
Andrew Gruner <andrew@divineflame.com>
2009-03-31 10:43:01 -0700

    renamed test.php to example.php

--- example.php
+++ example.php
@@ -1,11 +1,32 @@
 <?php
-error_reporting(E_WARNING);
+ini_set('display_errors', '1');
+#error_reporting(E_WARNING);
+error_reporting(E_ALL);
 $_section = 'Braces 101';
-$_page_name = 'Life with Braces';
+$_page_name = 'Life with Braces&reg;';
 $_keyword = 'Invisalign';
 $_page_title = '[this text replaces the base title]';
 $_alt = 'this string will be the default alt text when using the place_image() function';
+
+$logr = array();
+
+function logr($str)
+{
+  global $logr;
+  $logr[] = $str;
+}
+
 require_once('global.php');
+
+function test($code, $comments='')
+{
+  print "<h3 class=\"code\">$code</h3>\n";
+  if($comments){print "<p class=\"quiet\">$comments</p>\n";}
+  print "<p>\n";
+  eval("$code;");
+  print "</p>\n<hr/>\n";
+}
+
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -13,97 +34,96 @@ require_once('global.php');
 	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 	<?php print_meta_tags(); ?>
 	<link href="example.css" media="screen" rel="stylesheet" type="text/css" />
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js" type="text/javascript"></script>
 	<?php print_page_title(); ?>
+	<script type="text/javascript">
+    $(document).ready(function(){
+  		$('li.first').append('<span class="quiet">  < first</span>');
+  		$('li.last').append('<span class="quiet">  < last</span>');
+  		$('#nav-with-sub a[href^="http://"]').after('<span class="quiet"> (external)</span>');
+  		//$('a.head').after('<span class="quiet"> < head </span>');
+  	});//end document.ready
+  </script>
 </head>
 <body class="<?php echo slug_name($_page_name); ?>">
 
 <h1>PHP Framework</h1>
 
+<p>This is an example page illustrating the available php functions that utilize the site stucture defined in <code>sitemap.php</code>.</p>
 
+<p>Using our existing approach of defining <code>$page</code> and <code>$section</code> variables for each page (The framework uses the names <code>$_page_name</code>, and <code>$_section</code>), we can call the following functions from each page and get dynamicly generated navigation elements.</p>
 
-<h2>Navigation</h2>
+<p>These functions are defined in <code>global.php</code>. To enable this functionality simply include <code>global.php</code> on every page in addition to the header and footer includes. Then define the site structure as an array in <code>sitemap.php</code>, also kept in the includes folder. (<strong>Note:</strong> The root level &#8216;Site Map&#8217; page should be named <code>site-map.php</code> to avoid conflicting names.)</p>
 
-<pre><code>navigation($exclusions, $include_sub_nav, $div_id');</code></pre>
-<p>This function returns a <code>&lt;ul&gt;</code> with list items of the top level navigation links.</p>
-<p>For styling purposes, each top-level &lt;a&gt; is given an id  named after the section. It also adds a <code>class="active"</code> to the <code>&lt;li&gt;</code> of the current section.</p>
+<h2>Naming Conventions</h2>
 
-<h3>Options</h3>
+<p>'Slug Name' is a term used in blogging and CMS software for stripping spaces and special characters from a page title to use as a file name or in a URL. The framework relies on php files that follow this naming contention. (e.g. A page titled 'Life with Braces' would be named 'life-with-braces.php').</p>
 
-<ul>
-  <li>If you don't want certain sections to be included in the generated navigation, create an array with the section names you wish to exclude and pass them to the function as the <code>$exclusions</code> parameter.</li>
-  <li>$sub_nav is a boolean parameter that specifies if the sub nav should be included as a nested &lt;ul&gt;s.</li>
-  <li>The $div_id parameter lets you specify the id of the generated &lt;ul&gt;. It defaults to 'nav' if nothing is given.</li>
-</ul>
+<h2>Examples</h2>
 
-<h3>Example:</h3>
-<pre><code>navigation($exclusions = array('Contact Us','Site Map'));</code></pre>
-<?php print_navigation($exclusions = array('Contact Us','Site Map')); ?>
+<p>This page lists PHP commands as headers followed by the output of that exact code. The "&lt; first" and "&lt; last" tags are being added with jQuery to li.first and li.last so you don't have to view the source to see the class names. A similar visualization is being done with external links. The .active class is highlighted in yellow.</p>
 
-<pre><code>navigation($exclusions = array('Contact Us','Site Map'), $include_sub_nav=true, $div_id='nav-with-sub');</code></pre>
-<?php print_navigation($exclusions = array('Contact Us','Site Map'), $include_sub_nav=true, $div_id='nav-with-sub'); ?>
+<p>To illustrate these examples, this example page has the variables <code>$_section='Braces 101'</code> and <code>$_page_name='Life with Braces'</code>.</p>
 
 <hr />
-<h2>Sub Navigation</h2>
 
-<p>This function returns a <code>&lt;ul&gt;</code> with list items of the current section&#8217;s sub-navigation links. It also adds <code>class="active"</code> to the <code>&lt;li&gt;</code> of the current page.</p>
+<?php 
 
-<h3>Example:</h3>
+test("print_navigation(\$exclusions = array('Contact Us', 'Site Map'))");
+test("print_navigation(\$exclusions = array('Contact Us', 'Site Map'), \$include_sub_nav=true, \$div_id='nav-with-sub')");
 
-<pre><code>sub_navigation();</code></pre>
-<?php sub_navigation(); ?>
+test("print_inclusive_navigation(array('Home','Patient Login'))", 'print a navigation div with only the specified nav items, excludes everything else');
 
-<hr />
-<h2>Sub Navigation as a Paragraph</h2>
+test("print_sub_navigation()");
+test("print_sub_navigation(\$section='Nested')");
+test("print_sub_navigation_with_heading()");
+test("print_sub_navigation_with_heading('', \$link=true)", 'adds a linked heading');
+test("print_sub_navigation_with_heading('', '', 'h1')");
 
-<pre><code>sub_nav_p();</code></pre>
-<p>This function returns a <code>&lt;p&gt;</code> with the current section&#8217;s sub-navigation links. It also adds an <code>class="active"</code> to the <code>&lt;li&gt;</code> of the current page. The text or character inserted between each link can be customized.</p>
+test("sub_nav_p()");
+test("sub_nav_p(2)");
+test("sub_nav_p(array(2))");
+test("sub_nav_p(array(1,3))", 'break the string after the first and third items');
+test("sub_nav_p('', ' >>>> ')", 'change the text that separates items');
+test("sub_nav_p(array(3),' &bull; ')");
 
-<h3>Example:</h3>
+test("print_text_navigation()");
+test("print_text_navigation(4)");
+test("print_text_navigation(array(4))");
+test("print_text_navigation(array(2,5))");
+test("print_text_navigation(0, \$exclude=array('Contact Us'))");
+test("print_text_navigation(4, \$exclude=array('Test'), ' **** ')");
 
-<pre><code>sub_nav_p();</code></pre>
-<?php sub_nav_p(); ?>
+test("print_sitemap()");
+test("print_sitemap(\$exclude=array('Contact Us', 'About Orthodontics'))");
 
-<pre><code>sub_nav_p(array(3,7)); # split the list after the 3rd and 7th links</code></pre>
-<?php sub_nav_p(array(3,7)); ?>
+test("print_breadcrumbs()");
 
-<pre><code>sub_nav_p(array(5),' &amp;bull; '); # add custom separator string between links</code></pre>
-<?php sub_nav_p(array(5),' &bull; '); ?>
+test('echo get_site_name()', 'gets the value of "site_name" defined in config.php'); 
 
-<hr />
-<h2 class="clear">Text Navigation</h2>
-<p>This function returns a <code>&lt;p&gt;</code> string listing the top level navigation items as links separated by 'pipe' characters. The second example shows how you can optionally force a break after a specific number of links.</p>
+$slug_tests = array('TMJ/TMD', 'Invisalign&reg;', 'Damon&trade;', 'Why Braces?');
+foreach ($slug_tests as $test){
+  test("echo slug_name('$test')");
+}
 
-<h3>Example:</h3>
-<pre><code>text_navigation();</code></pre>
-<?php text_navigation(); ?>
+test('place_image("alf.jpg")');
+test('place_image("alf")', 'works without the file extension if there is a corresponding gif, jpg, or png');
+test('place_image("test-png")');
+test('place_image("test-gif")');
+test('place_image("test-jpg")');
+test('place_image("non-existing-file")', "doesn't output anything if it can't find the file");
 
-<pre><code>text_navigation(4); # break the line after the forth link</code></pre>
-<?php text_navigation(4); ?>
 
-<hr />
-<h2>Sitemap</h2>
-<p>This function parses <code>sitemap.php</code> and creates nested <code>&lt;ul&gt;</code>s of the site structure with links to each page.</p>
-<h3>Example:</h3>
-<pre><code>sitemap();</code></pre>
-<?php sitemap(); ?>
+?>
 
-<hr />
-<h2>Breadcrumbs</h2>
-<h3>Example:</h3>
-<pre><code>breadcrumbs();</code></pre>
-<?php breadcrumbs(); ?>
+<!--<h2>print_r($sitemap)</h2>-->
+<?php
+  # $sitemap = parse_sitemap();
+	# print_r($sitemap);
+?>
 
-<hr />
-<h2>Place Image</h2>
-<p>This function generates an image tag with calculated width and height attributes as well as an alt tag read from an '_alt' variable at the top of the page.</p>
-<h3>Options</h3>
-<ul>
-  <li>Specify the alt text</li>
-  <li>Specify the class name given to the image</li>
-</ul>
-<h3>Example:</h3>
-<pre><code>place_image("filename.jpg", ["alt text", "class_name"]);</code></pre>
-<?php place_image("alf.jpg"); ?>
+<!--<h2>Log</h2>-->
+<?php # print_r($logr); ?>
 
 </body>
 </html>
