942c266347ac807aac41e5010b8b7cf5d0e4a21d
Andrew Gruner <andrew@divineflame.com>
2011-02-28 16:12:55 -0800

    Moved license out of doozer.php and into its own file

--- lib/doozer/doozer.php
+++ lib/doozer/doozer.php
@@ -13,43 +13,15 @@
  *  @copyright Copyright (c) 2009 Andrew Gruner
  *  @license http://opensource.org/licenses/mit-license.php The MIT License
  *  @package doozer
- *  @version 2.1.0
+ *  @version 2.1.1
  */
-
-#------------------------------------------------------------------------------#
-#    Copyright (c) 2009 Andrew Gruner                                          #
-#                                                                              #
-#    Permission is hereby granted, free of charge, to any person               #
-#    obtaining a copy of this software and associated documentation            #
-#    files (the "Software"), to deal in the Software without                   #
-#    restriction, including without limitation the rights to use,              #
-#    copy, modify, merge, publish, distribute, sublicense, and/or sell         #
-#    copies of the Software, and to permit persons to whom the                 #
-#    Software is furnished to do so, subject to the following                  #
-#    conditions:                                                               #
-#                                                                              #
-#    The above copyright notice and this permission notice shall be            #
-#    included in all copies or substantial portions of the Software.           #
-#                                                                              #
-#    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,           #
-#    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES           #
-#    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND                  #
-#    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT               #
-#    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,              #
-#    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING              #
-#    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR             #
-#    OTHER DEALINGS IN THE SOFTWARE.                                           #
-# ============================================================================ #
-
-
-
 
 /*
   Include the configuration and sitemap files
 
   These define site-specific settings
   such as page title and meta information. This allows for site-specific changes
-  without having to edit global.php.
+  without having to edit this file.
 */
 require_once('config.php');
 require_once('sitemap.php');
